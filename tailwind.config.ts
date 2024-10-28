56a50b01a8b5f81df1192cd225b97e36cece32fa
Maor <maorsa9@gmail.com>
2024-10-28 13:43:19 +0200

    first commit

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,10 +7,29 @@ const config: Config = {
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    container: {
+      center: true,
+      padding: "15px",
+    },
+    screens: {
+      sm: "640px",
+      md: "768px",
+      lg: "1024px",
+      xl: "1200px",
+    },
+
+    fontFamily: {},
+
     extend: {
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
+        primary: "#10151f",
+        secondary: "#475467",
+        accent: {
+          DEFAULT: "#7edad2",
+          hover: "#79d3cc",
+        },
       },
     },
   },
