ac8e31b9e30b3f59b54ba6b0c9951529589d893e
Maor <maorsa9@gmail.com>
2024-11-11 19:18:27 +0200

    start working on the dark mode

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -5,7 +5,7 @@ const Footer = () => {
     <div className="py-12 bg-primary">
       <div className="container mx-auto">
         <div className="flex flex-col xl:flex-row items-center justify-between">
-          <Logo light={true} />
+          <Logo />
           <div className="text-white/70 font-light">
             Copyright &copy; 2024. All rights reserved.
           </div>
